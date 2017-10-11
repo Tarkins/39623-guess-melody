3d47a77163a35391fca7b3134cf52b241729b129
Tarkins <tarasovartyomand@mail.ru>
2017-10-12 00:04:18 +0300

    Добавлено переключение экранов

---
+++ js/main.js
@@ -0,0 +1,41 @@
+'use strict';
+const template = document.querySelector(`template`).content;
+const sections = template.querySelectorAll(`.main`);
+const mainScreen = document.querySelector(`.app`);
+
+let currentScreenNum = 0;
+
+
+
+const changeScreen = (num) => {
+  mainScreen.replaceChild(sections[num], mainScreen.firstElementChild);
+}
+
+changeScreen(currentScreenNum);
+
+
+const swipeScreen = (direction) => {
+  if(direction === "right" && currentScreenNum < sections.length-1) {
+    currentScreenNum++;
+    changeScreen(currentScreenNum);
+  } else if (direction === "left" && currentScreenNum > 0) {
+    currentScreenNum--;
+    changeScreen(currentScreenNum);
+  }
+}
+
+document.addEventListener(`keydown`, (key) => {
+  if(!key.altKey) return;
+  else if (key.altKey && key.keyCode === 39) {
+    swipeScreen("right");
+} else if (key.altKey && key.keyCode === 37) {
+    swipeScreen("left");
+}
+})
+
+
+
+
+
+
+
