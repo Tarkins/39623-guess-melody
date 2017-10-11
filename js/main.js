1954ce7d28bde3437ef05318bed26ba4e7d80c08
Tarkins <tarasovartyomand@mail.ru>
2017-10-12 00:25:48 +0300

    Исправления eslint

--- js/main.js
+++ js/main.js
@@ -1,41 +1,32 @@
-'use strict';
 const template = document.querySelector(`template`).content;
 const sections = template.querySelectorAll(`.main`);
 const mainScreen = document.querySelector(`.app`);
 
 let currentScreenNum = 0;
 
-
-
 const changeScreen = (num) => {
   mainScreen.replaceChild(sections[num], mainScreen.firstElementChild);
-}
+};
 
 changeScreen(currentScreenNum);
 
 
 const swipeScreen = (direction) => {
-  if(direction === "right" && currentScreenNum < sections.length-1) {
+  if (direction === `right` && currentScreenNum < sections.length - 1) {
     currentScreenNum++;
     changeScreen(currentScreenNum);
-  } else if (direction === "left" && currentScreenNum > 0) {
+  } else if (direction === `left` && currentScreenNum > 0) {
     currentScreenNum--;
     changeScreen(currentScreenNum);
   }
-}
+};
 
 document.addEventListener(`keydown`, (key) => {
-  if(!key.altKey) return;
-  else if (key.altKey && key.keyCode === 39) {
-    swipeScreen("right");
-} else if (key.altKey && key.keyCode === 37) {
-    swipeScreen("left");
-}
-})
-
-
-
-
-
-
-
+  if (!key.altKey) {
+    return;
+  } else if (key.altKey && key.keyCode === 39) {
+    swipeScreen(`right`);
+  } else if (key.altKey && key.keyCode === 37) {
+    swipeScreen(`left`);
+  }
+});
