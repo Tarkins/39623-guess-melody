afa65b17923f6de67ae89d18daf5ec2fb07dc905
Tarkins <tarasovartyomand@mail.ru>
2017-10-13 02:52:24 +0300

    Сборка зависимостей. Модуль, возвращающий dom элемент

--- js/makeElementFromTemplate.js
+++ js/makeElementFromTemplate.js
@@ -2,6 +2,6 @@ export const makeElementFromTemplate = (nodeHtml) => {
   const template = document.createElement(`template`);
   template.innerHTML = nodeHtml;
   return template.content;
-}
+};
 
 export default makeElementFromTemplate;
